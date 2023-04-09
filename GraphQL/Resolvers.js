04d6e43d2617ec25d833bcf2495ff93865cf690f
bocchuoi <65423244+bocchuoi@users.noreply.github.com>
2023-04-09 08:56:42 -0400

    Add files via upload

---
+++ GraphQL/Resolvers.js
@@ -0,0 +1,93 @@
+const empModel = require("../models/EmployeeModel")
+const userModel = require("../models/UserModel")
+
+
+const resolvers = {
+    Query: {
+        async getAllEmployees() {
+            return await empModel.find()
+        },
+
+        async login(parent, args) {
+            const user = await userModel.findOne({username:args.username})
+            if (user && user.password === args.password) {
+                return "Logged in"
+            }
+            else {
+                return "You shall not pass!"
+            }
+        },
+
+        async getEmployee(parent, args) {
+            return await empModel.findOne({_id:args.eid})
+        },
+
+        async getEmails() {
+            return await empModel.find().select({email: 1})
+        }
+    },
+
+    Mutation: {
+        async signup(parent, args) {
+            try {
+                const newUser = new userModel({username:args.username, password:args.password, email:args.email})
+                await newUser.save()
+                return `Account ${args.username} Registered`
+            }
+            catch(e) {
+                console.log(e)
+                return "Account was not registered. Please check error log!"
+            }
+
+
+        },
+        async addEmployee(parent, args) {
+            try {
+                const newEmp = empModel(args.employee)
+                await newEmp.save()
+                // return `The employee ${args.employee.firstName} ${args.employee.lastName} is added`
+                return newEmp
+
+            }
+            catch(e) {
+                console.log(e)
+                return "Employee was not added. Please check error log!"
+            }
+        },
+        async updateEmployee(parent, args) {
+            try {
+                // args.employee._id = args.eid
+                const updatedEmp = await empModel.findByIdAndUpdate(args.employee._id, args.employee)
+                if (updatedEmp) {
+                    const nb = await updatedEmp.save()
+                    return nb
+                    // return `The employee ${args.employee.firstName} ${args.employee.lastName} is updated`
+                }
+                else {
+                    return `Couldn't find the employee with ${args.eid} id`
+                }
+        
+            } catch (e) {
+                console.log(e)
+                return "Employee was not updated. Please check error log!"
+            }
+        },
+        async deleteEmployee(parent, args) {
+            try {
+                const deletedEmp = await empModel.findByIdAndDelete(args.eid)
+                if (deletedEmp) {
+                    return deletedEmp
+                    return `Deleted employee with ${args.eid} id`
+                }
+                else {
+                    return `Couldn't find the employee with ${args.eid} id`
+                }
+            } catch (e) {
+                console.log(e)
+                return "Employee was not deleted. Please check error log!"
+            }
+        }
+    }
+}
+
+module.exports = resolvers
